cd2bcec01c4e97e4509179621993104179ec8b96
OUADEIH Larbi <121357277+larbi01@users.noreply.github.com>
2025-05-15 18:42:25 +0200

    Update BookRepository.java

--- book-service/src/main/java/com/example/bookservice/BookRepository.java
+++ book-service/src/main/java/com/example/bookservice/BookRepository.java
@@ -3,6 +3,6 @@ package com.example.bookservice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository  // 👈 Optional but safe to force detection
+@Repository 
 public interface BookRepository extends JpaRepository<Book, Long> {}
 
